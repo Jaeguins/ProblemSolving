5d3d03607bcf825a6cbc8d65982536629c9f31fb
Jaeguins <jsu4838@naver.com>
2018-09-13 16:53:54 +0900

    datStruct 2_3 modified

--- 자료구조/Ctest/2_3.c
+++ 자료구조/Ctest/2_3.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #define MAX_WORD_LENGTH 50
 #define MAX_WORD_PER_LINE 80
-#define CHARACTER_PER_LINE 80
+#define CHARACTER_PER_LINE 40
 char line[80][50];
 int main() {
     FILE *read, *write;
@@ -11,16 +11,16 @@ int main() {
     fopen_s(&write, "aligned.txt", "w");
     int wInd = 0, lLength = 0;
     while (fscanf_s(read, "%s ", &buffer, MAX_WORD_LENGTH) != EOF) {
-        lLength += strlen(buffer) + 1;
-        if (lLength > CHARACTER_PER_LINE) {
-            for (int i = 0; i < wInd; i++) {
-                fprintf(write,"%s", line[i]);
-                fprintf(write," ");
+        if (lLength+strlen(buffer) > CHARACTER_PER_LINE-wInd) {
+            for (int i = 0; i < wInd-1; i++) {
+                fprintf(write,"%s ", line[i]);
             }
-            wInd = 0;
-            lLength = strlen(buffer)+1;
+            fprintf(write,line[wInd - 1]);
             fprintf(write,"\n");
+            wInd = 0;
+            lLength = 0;
         }
+        lLength += strlen(buffer);
         strcpy_s(line[wInd++], 50, buffer);
     }
     for (int i = 0; i < wInd; i++) {
