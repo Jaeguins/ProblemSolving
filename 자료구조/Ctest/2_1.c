0c1488d4d747f5f080b404b24fa0d1beae36f066
Jaeguins <jsu4838@naver.com>
2018-09-17 15:35:55 +0900

    2_1 integer under 0 exception fixed

--- 자료구조/Ctest/2_1.c
+++ 자료구조/Ctest/2_1.c
@@ -2,19 +2,25 @@
 #define MAX_INPUT_LINE 120//최대 입력 문자열 수
 int main() {
     int tData = 0, dataInd = 0;
+    char minused = 0;
     char buffer[MAX_INPUT_LINE];
     int sum = 0;
     gets_s(&buffer, MAX_INPUT_LINE);//buffer에 다 밀어넣고
     for (int i = 0; i < sizeof(buffer); i++) {//한글자씩 뽑아서
         if (buffer[i] < 58 && buffer[i]>47) {//숫자인가?
-            if (tData > 0)
+            if (tData != 0)
                 tData *= 10;//이전에 있던 숫자가 있으면 한자리 올려주고
             tData += (buffer[i] - 48);//ASCII -> 십진수 전환 후 더함
         }
+        else if (buffer[i] == '-') {//음수이면
+            minused = 1;//빼기 트리거 On
+        }
         else {//숫자가 아니면
-            if (tData > 0) {//전글자까지가 숫자였다면
-                sum += tData;//누산 후 숫자 초기화
-                tData = 0;
+            if (tData != 0||minused) {//전글자까지가 숫자였다면
+                if (minused)sum -= tData;//음수라면 빼고
+                else sum += tData;//양수라면 더하고
+                tData = 0;//재초기화
+                minused = 0;
             }
         }
     }
